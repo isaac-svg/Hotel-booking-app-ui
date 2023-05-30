0d4e2ae0497b8eb8221dcf4e9f74a8506fa53ed1
isaac-svg <isaacsakyi009@gmail.com>
2023-05-30 22:32:14 +0000

    hotel booking site ui

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">bookon</span>
+        </Link>
+        {user ? user.username : (
+          <div className="navItems">
+            <button className="navButton"><Link to="/register"  style={{textDecoration:"none",color:"inherit"}}>Register</Link></button>
+
+            <button className="navButton"><Link to="/login" style={{textDecoration:"none"}}>Login</Link></button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
