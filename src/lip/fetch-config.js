48d9714cf22ef60399aeccedddfecb011feb659d
isaac-svg <isaacsakyi009@gmail.com>
2023-11-13 06:09:14 +0000

    Update base URL

--- src/lip/fetch-config.js
+++ src/lip/fetch-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'https://hotel-booking-api-rho.vercel.app/api', 
-  baseURL:"http://localhost:8080/api"
+  baseURL: 'https://hotel-booking-api-rho.vercel.app/api', 
+  // baseURL:"http://localhost:8080/api"
 
 });
